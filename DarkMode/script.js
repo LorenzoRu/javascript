833ad263341a032ab5ebd16b066a14d6ef6c786c
unknown <lorenzo.ruocco9219@gmaim.com>
2022-02-20 19:00:05 +0100

    add display mode

---
+++ DarkMode/script.js
@@ -0,0 +1,30 @@
+console.log("connected");
+
+const switchBox = document.querySelector('.switch');
+console.log(switchBox);
+const btn = document.querySelector('.btn');
+console.log(btn);
+const icone = document.querySelector('i');
+console.log(icone);
+const container = document.querySelector('.container');
+console.log(container);
+const title = document.querySelector('.title');
+console.log(title);
+
+
+switchBox.addEventListener('click', function() {
+    console.log("switch clicked");
+    btn.classList.toggle('btn-changed');
+    icone.classList.toggle('icone-change');
+    icone.classList.toggle('fa-sun');
+    switchBox.classList.toggle('switch-changed');
+    container.classList.toggle('container-changed');
+
+    if(title.innerText === "Mode Sombre"){
+        title.innerText = "Mode Clair";
+        title.classList.add('title-changed');
+    }else{
+        title.innerText = "Mode Sombre";
+        title.classList.remove('title-changed');
+    };
+});
